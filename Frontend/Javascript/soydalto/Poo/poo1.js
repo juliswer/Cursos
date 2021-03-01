70bc1448c6ea8c909c5c37f1c6a164e73f347863
Julián Swerdlin <julianswer36@gmail.com>
2021-03-01 14:02:53 -0300

    Add files via upload

--- Frontend/Javascript/soydalto/Poo/poo1.js
+++ Frontend/Javascript/soydalto/Poo/poo1.js
@@ -5,12 +5,19 @@ class animal {
         this.color = color;
         this.info = `Soy ${this.especie}, tengo ${this.edad} años y soy de color ${this.color}`;
     }
+    verInfo(){
+        document.write(this.info) + "<br>";
+    }
 }
 
 let perro = new animal ("perro", 5, "marron")
 let gato = new animal ("gato", 2, "negro");
 let pajaro = new animal ("pajaro", 1, "verde");
 
-document.write(perro.info + "<br>")
-document.write(gato.info + "<br>")
-document.write(pajaro.info + "<br>")
+// document.write(perro.info + "<br>")
+// document.write(gato.info + "<br>")
+// document.write(pajaro.info + "<br>")
+
+perro.verInfo();
+gato.verInfo();
+pajaro.verInfo();
