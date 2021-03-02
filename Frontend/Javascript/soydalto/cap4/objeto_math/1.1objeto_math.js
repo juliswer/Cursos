07c7470ea4464049a054b5fe2774fa6f3cd4e00e
Julián Swerdlin <julianswer36@gmail.com>
2021-03-02 11:34:14 -0300

    Add files via upload

---
+++ Frontend/Javascript/soydalto/cap4/objeto_math/1.1objeto_math.js
@@ -0,0 +1,10 @@
+let numero = Math.PI;
+let numero = Math.SQRT1_2;
+let numero = Math.SQRT2;
+let numero = Math.E;
+let numero = Math.LN2;
+let numero = Math.LN10;
+let numero = Math.LOG2E;
+let numero = Math.LOG10E;
+
+document.write(numero);
