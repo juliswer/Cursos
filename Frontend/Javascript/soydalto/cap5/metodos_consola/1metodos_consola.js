636097b92560efe6cec210780ebaa48be15a5d73
Julián Swerdlin <julianswer36@gmail.com>
2021-03-02 19:09:50 -0300

    Add files via upload

--- Frontend/Javascript/soydalto/cap5/metodos_consola/1metodos_consola.js
+++ Frontend/Javascript/soydalto/cap5/metodos_consola/1metodos_consola.js
@@ -1,1 +1,8 @@
-alert("hola")
+console.assert(5 < 10)
+console.clear
+console.error("no capo")
+console.info("muy buen trabajo")
+console.log("muy buen trabajo")
+console.table([5,3,6,8,9])
+console.warn("esta bien, pero cuidado")
+console.dir([1,2,3,4,5])
