2ad96016047d0a620c84aebb93917471cdbee2a9
Julián Swerdlin <julianswer36@gmail.com>
2021-03-02 20:21:54 -0300

    Add files via upload

--- Frontend/Javascript/soydalto/cap5/cofla5/cofla5.1.js
+++ Frontend/Javascript/soydalto/cap5/cofla5/cofla5.1.js
@@ -3,3 +3,24 @@ let estudio = "100 minutos estudio";
 let tp = "100 minutos hacer trabajos practicos";
 let homework = "30 minutos tareas del hogar";
 let descanso = "10 minutos de descanso";
+
+console.log("TAREAS");
+for (var i = 0; i < 14; i++) {
+	if (i == 0) {
+		console.group("semana 1")
+	}
+    console.groupCollapsed("dia " + (i+1));
+    console.log(trabajo);
+    console.log(descanso);
+    console.log(estudio);
+    console.log(tp);
+    console.log(homework);
+    console.groupEnd();
+if (i == 7) {
+	console.groupEnd();
+	console.groupCollapsed("semana 2")
+}
+}
+
+console.groupEnd();
+console.groupEnd();
