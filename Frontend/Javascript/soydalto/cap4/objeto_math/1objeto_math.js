5754325bd853054dc2bac68ee27518db4ea2b379
Julián Swerdlin <julianswer36@gmail.com>
2021-03-02 11:27:19 -0300

    Add files via upload

---
+++ Frontend/Javascript/soydalto/cap4/objeto_math/1objeto_math.js
@@ -0,0 +1,13 @@
+numero = Math.sqrt(25);
+numero = Math.cbrt(27);
+numero = Math.max(4,1,6,7,8,45,98);
+numero = Math.min(1,2,3,54,5,6);
+numero = Math.random();
+numero = Math.fround(9.76);
+numero = Math.trunc(9.9);
+
+let numero = Math.random()*99;
+numero = Math.round(numero);
+numero = Math.floor(numero); //forma mas optima
+
+document.write(numero)
