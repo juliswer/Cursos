b2897ff29ca6c242d0c632e59265629d4aada683
Julián Swerdlin <julianswer36@gmail.com>
2021-03-04 20:34:09 -0300

    Add files via upload

---
+++ Frontend/Javascript/soydalto/cap6/atributos_globales/atributos_globales.js
@@ -0,0 +1,7 @@
+const titulo = document.querySelector(".titulo");
+
+titulo.setAttribute("contentEditable", "true")
+titulo.setAttribute("dir", "ltr")
+titulo.setAttribute("hidden", "true")
+titulo.setAttribute("tabindex", "0")
+titulo.setAttribute("title", "hola")
