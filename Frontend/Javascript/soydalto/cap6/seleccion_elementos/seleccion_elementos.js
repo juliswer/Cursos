1446e481ce7eeb8def9bb8f24d8a958a769fcf20
Julián Swerdlin <julianswer36@gmail.com>
2021-03-04 20:35:52 -0300

    Add files via upload

---
+++ Frontend/Javascript/soydalto/cap6/seleccion_elementos/seleccion_elementos.js
@@ -0,0 +1,7 @@
+parrafo = document.getElementById("parrafo");
+
+parrafo = document.getElementsByTagName("p");
+
+parrafo = document.querySelector("#parrafo")
+
+parrafo = document.querySelectorAll("#parrafo")
