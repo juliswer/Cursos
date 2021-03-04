9627d509881635bafebfdcffec3943bc0bd0c523
Julián Swerdlin <julianswer36@gmail.com>
2021-03-04 20:34:44 -0300

    Add files via upload

---
+++ Frontend/Javascript/soydalto/cap6/atributos_inputs/atributos_inputs.js
@@ -0,0 +1,10 @@
+const input = document.querySelector(".input-normal");
+
+input.className()
+input.nodeValue()
+input.nodeType()
+input.nodeAccept()
+input.nodeForm()
+input.minLength = "43";
+input.placeholder = "hola";
+input.required = "required";
