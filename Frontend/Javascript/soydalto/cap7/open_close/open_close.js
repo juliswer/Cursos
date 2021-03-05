f26cd10fa078fb9256819d3fff891637eaae7fca
Julián Swerdlin <julianswer36@gmail.com>
2021-03-05 14:12:29 -0300

    Add files via upload

--- Frontend/Javascript/soydalto/cap7/open_close/open_close.js
+++ Frontend/Javascript/soydalto/cap7/open_close/open_close.js
@@ -1,3 +1,7 @@
 let ventanaAbierta = window.open("https://google.com")
 
 ventanaAbierta.close();
+
+ventanaAbierta.closed(); //devuelve valor booleano
+
+ventanaAbierta.stop(); //deja de cargar lo que cargaba
