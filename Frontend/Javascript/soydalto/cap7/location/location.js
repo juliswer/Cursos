012b1c73fca1972f07eb8ae5e25264f352bfe75b
Julián Swerdlin <julianswer36@gmail.com>
2021-03-05 14:51:54 -0300

    Add files via upload

---
+++ Frontend/Javascript/soydalto/cap7/location/location.js
@@ -0,0 +1,5 @@
+const href = window.location.href; 
+const hostname = window.location.hostname;
+const pathname = window.location.pathname;
+const protocol = window.location.protocol;
+const assign = window.location.assign;
