8760f67bb3661f8fc31da5f266228c824791cd80
Julián Swerdlin <julianswer36@gmail.com>
2021-03-05 14:40:47 -0300

    Add files via upload

---
+++ Frontend/Javascript/soydalto/cap7/move_resize/move_resize.js
@@ -0,0 +1,6 @@
+window.minimize(); //esta funcion ya no tiene soporte, por ende, no funciona mas
+
+window.resizeBy(20,200)
+window.resizeTo(10,140);
+window.moveBy(100,300);
+window.moveTo(100,200);
