be92fb4369ace4a1ea83a977a2829a48c8cc4334
Julián Swerdlin <julianswer36@gmail.com>
2021-03-05 09:07:49 -0300

    Add files via upload

---
+++ Frontend/Javascript/soydalto/cap6/obtencion_modificacion_elementos/obtencion_modificacion_elementos.js
@@ -0,0 +1,7 @@
+const titulo = document.querySelector(".titulo");
+
+let resultado = titulo.textContent;
+let resultado2 = titulo.innerText;
+let resultado3 = titulo.outerText;
+let resultado4 = titulo.innerHTML;
+let resultado5 = titulo.outerHTML;
