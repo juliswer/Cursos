eb688b9dfc3786f059ac5114872c3ef7a8b8835e
Julián Swerdlin <julianswer36@gmail.com>
2021-03-05 14:36:10 -0300

    Add files via upload

---
+++ Frontend/Javascript/soydalto/cap7/screen_scroll/screen_scroll.js
@@ -0,0 +1,6 @@
+const screen = window.screen;
+const screenLeft = window.screenLeft;
+const screenTop = window.screenTop;
+const scrollX = window.scrollX;
+const scrollY = window.scrollY;
+const scroll = window.scroll(0,100);
