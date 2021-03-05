956f8072696c44034ee0748ffb2ae7380e368e34
Julián Swerdlin <julianswer36@gmail.com>
2021-03-05 10:15:31 -0300

    Add files via upload

---
+++ Frontend/Javascript/soydalto/cap6/metodo_parents/metodo_parents.js
@@ -0,0 +1,6 @@
+const contenedor = document.querySelectorAll(".contenedor");
+
+const parrafo = document.createElement("P").innerHTML = "Parrafo";
+const h2_nuevo = document.createElement("H2");
+
+const h2_antiguo = document.querySelector(".h2");
