c773f32ee31420808afcd02317820e5c0a03f2ef
Julián Swerdlin <julianswer36@gmail.com>
2021-03-05 09:57:03 -0300

    Add files via upload

---
+++ Frontend/Javascript/soydalto/cap6/obtencion_modificacion_childs/obtencion_modificacion_childs.js
@@ -0,0 +1,9 @@
+const contenedor = document.querySelector(".contenedor");
+
+const primerHijo = contenedor.firstElementChild;
+
+const ultimoHijo = contenedor.lastElementChild;
+
+const hijos = contenedor.childNodes;
+
+const hijos2 = contenedor.children;
