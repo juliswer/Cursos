04bb6250d756e6ee4d18f946a17181ee8cf4bb7c
Julián Swerdlin <julianswer36@gmail.com>
2021-03-09 19:02:43 -0300

    Add files via upload

---
+++ Frontend/Javascript/soydalto/cap9/mouse_events/mouse_events.js
@@ -0,0 +1,51 @@
+const button = document.querySelector(".button");
+
+button.addEventListener("click", (e) => {
+    alert("di click pero en el boton")
+    e.stopPropagation()
+});
+
+button.addEventListener("dblclick", (e) => {
+    alert("di click pero en el boton")
+    e.stopPropagation()
+});
+
+button.addEventListener("mouseover", (e) => {
+    alert("di click pero en el boton")
+    e.stopPropagation()
+});
+
+button.addEventListener("mouseout", (e) => {
+    alert("di click pero en el boton")
+    e.stopPropagation()
+});
+
+button.addEventListener("contextmenu", (e) => {
+    alert("di click pero en el boton")
+    e.stopPropagation()
+});
+
+button.addEventListener("mouseenter", (e) => {
+    alert("di click pero en el boton")
+    e.stopPropagation()
+});
+
+button.addEventListener("mouseleave", (e) => {
+    alert("di click pero en el boton")
+    e.stopPropagation()
+});
+
+button.addEventListener("mousedown", (e) => {
+    alert("di click pero en el boton")
+    e.stopPropagation()
+});
+
+button.addEventListener("mouseup", (e) => {
+    alert("di click pero en el boton")
+    e.stopPropagation()
+});
+
+button.addEventListener("mousemove", (e) => {
+    alert("di click pero en el boton")
+    e.stopPropagation()
+});
