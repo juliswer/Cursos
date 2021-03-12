d72e54f52640d4e300a3d68698eb4b796c45a093
Julián Swerdlin <julianswer36@gmail.com>
2021-03-12 14:29:52 -0300

    Add files via upload

--- python/ruamlex/poo.py
+++ python/ruamlex/poo.py
@@ -2,5 +2,16 @@ class Tesla:
     bateria = 0
     panel = 1
     modelo = "s"
-    def auto_conducir():
-        print("Me estoy conduciendo solapa")
+    def __init__(self, cargaBateria, modeloTesla):
+        self.bateria = cargaBateria
+        self.modelo = modeloTesla
+
+    def auto_conducir(self):
+        print("Soy modelo {} y tengo la capacidad de manejarme solo".format(self.modelo))
+        print("Quedan {} de bateria".format(self.bateria))
+
+teslaY = Tesla(100, "Y")
+teslaX = Tesla(200, "X")
+
+teslaX.auto_conducir()
+teslaY.auto_conducir()
