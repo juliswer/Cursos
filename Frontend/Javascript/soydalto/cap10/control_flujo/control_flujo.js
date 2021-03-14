a1d1aec0db727a48d84821856d7662a1de104584
Julián Swerdlin <julianswer36@gmail.com>
2021-03-14 13:59:27 -0300

    Add files via upload

---
+++ Frontend/Javascript/soydalto/cap10/control_flujo/control_flujo.js
@@ -0,0 +1,13 @@
+let nombre = "pedro"
+
+if (3 > 3){
+    alert(nombre);
+}
+
+// Ambito de bloque especifico
+{
+    let nombre = "pedro";
+    alert(nombre)
+}
+
+// las sentencias de control de flujo son if, else if, else o switch
