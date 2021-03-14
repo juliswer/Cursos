2705857577e138d4e4ee9f4ce474cbee1d0db865
Julián Swerdlin <julianswer36@gmail.com>
2021-03-14 14:57:24 -0300

    Add files via upload

---
+++ Frontend/Javascript/soydalto/cap10/switch/switch.js
@@ -0,0 +1,27 @@
+let expr = "carlitos"
+
+switch(expr){
+    case 1:
+        console.log("caso 1");
+        break;
+    case 2:
+        console.log("caso 2");
+        break;
+    case 3:
+        console.log("caso 3");
+        break;
+    default:
+        alert("No es ninguna")
+}
+
+if (expr == "carlitos"){
+    console.log("Caso 1")
+}
+
+else if (expr == "pepito"){
+    console.log("Caso 2")
+}
+
+else if (expr == "juancito"){
+    console.log("Caso 3")
+}
