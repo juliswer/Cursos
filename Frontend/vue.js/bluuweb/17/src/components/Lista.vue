1504957f50f29eb3351ff6b46d9a34d27d0f5666
Julián Swerdlin <julianswer36@gmail.com>
2021-03-27 12:18:49 -0300

    Add files via upload

---
+++ Frontend/vue.js/bluuweb/17/src/components/Lista.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <ul class="list-group">
+            <li v-bind:key="item" v-for="(item, index) of frutas" @click="aumentar(index)"
+            class="list-group-item d-flex justify-content-between align-items-center">
+                {{item.nombre}}
+                <span class="badge bg-primary rounded-pill">{{item.cantidad}}</span>
+            </li>
+            <button class="btn btn-danger btn-block" @click='reiniciar'>Reiniciar</button>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import {mapState, mapMutations} from 'vuex';
+
+    export default {
+        name: 'Lista',
+        computed: {
+            ...mapState(['frutas']),
+        },
+        methods: {
+            ...mapMutations(['aumentar', 'reiniciar'])
+        }
+    }
+</script>
