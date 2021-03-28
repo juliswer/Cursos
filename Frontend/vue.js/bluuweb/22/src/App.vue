2af70715e6147d73c2efb21ef2125719935f9d99
Julián Swerdlin <julianswer36@gmail.com>
2021-03-28 13:25:12 -0300

    Add files via upload

---
+++ Frontend/vue.js/bluuweb/22/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <b-navbar toggleable="lg" type="dark" variant="danger">
+      <b-container>
+            <b-navbar-brand href="#">NavBar</b-navbar-brand>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse is-nav id="nav_collapse">
+              <b-navbar-nav>
+                <b-nav-item :to="{name: 'home'}" exact>Home</b-nav-item>
+                <b-nav-item :to="{name: 'about'}">About</b-nav-item>
+              </b-navbar-nav>
+            </b-collapse>
+            
+      </b-container>
+    </b-navbar>
+    <router-view/>
+  </div>
+</template>
