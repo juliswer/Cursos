c2a4b5f3bb09c7bad37212c434e12d3151d9a9f3
Julián Swerdlin <julianswer36@gmail.com>
2021-03-28 18:27:39 -0300

    Add files via upload

---
+++ Frontend/vue.js/bluuweb/23/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <b-navbar toggleable="md" type="dark" variant="danger">
+      <b-container>
+            <b-navbar-brand :to="{name: 'Home'}">NasheCompany</b-navbar-brand>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse is-nav id="nav_collapse">
+              <b-navbar-nav>
+                <b-nav-item :to="{name: 'Home'}" exact>Home</b-nav-item>
+                <b-nav-item :to="{name: 'About'}">About</b-nav-item>
+                <b-nav-item :to="{name: 'Grid'}">Grid</b-nav-item>  
+              </b-navbar-nav>
+            </b-collapse>
+            
+      </b-container>
+    </b-navbar>
+    <b-container>
+      <router-view/>
+    </b-container>
+  </div>
+</template>
---
+++ Frontend/vue.js/bluuweb/23/src/views/Grid.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="mt-5">
+        <b-row>
+            <b-col cols="12" md="6" lg="4">
+                <img src="@/assets/logo.png" alt="">
+            </b-col>
+            <b-col cols="12" md="6" lg="8">
+                <p class="mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus minima hic repudiandae eaque fuga ab incidunt, illo esse soluta sed maxime consequuntur nesciunt dignissimos accusamus amet iusto voluptates consequatur?</p>
+            </b-col>
+            <b-col cols="12" md="6" lg="8" class="mt-5">
+                <p class="mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus minima hic repudiandae eaque fuga ab incidunt, illo esse soluta sed maxime consequuntur nesciunt dignissimos accusamus amet iusto voluptates consequatur?</p>
+            </b-col>
+            <b-col cols="12" md="6" lg="4" class="mt-5">
+                <img src="@/assets/logo.png" alt="">
+            </b-col>
+            <b-col cols="12" md="6" lg="4">
+                <img src="@/assets/logo.png" alt="">
+            </b-col>
+            <b-col cols="12" md="6" lg="8">
+                <p class="mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus minima hic repudiandae eaque fuga ab incidunt, illo esse soluta sed maxime consequuntur nesciunt dignissimos accusamus amet iusto voluptates consequatur?</p>
+            </b-col>
+            <b-col cols="12" md="6" lg="8" class="mt-5">
+                <p class="mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus minima hic repudiandae eaque fuga ab incidunt, illo esse soluta sed maxime consequuntur nesciunt dignissimos accusamus amet iusto voluptates consequatur?</p>
+            </b-col>
+            <b-col cols="12" md="6" lg="4" class="mt-5">
+                <img src="@/assets/logo.png" alt="">
+            </b-col>
+            <b-col cols="12" md="6" lg="4">
+                <img src="@/assets/logo.png" alt="">
+            </b-col>
+            <b-col cols="12" md="6" lg="8">
+                <p class="mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus minima hic repudiandae eaque fuga ab incidunt, illo esse soluta sed maxime consequuntur nesciunt dignissimos accusamus amet iusto voluptates consequatur?</p>
+            </b-col>
+            <b-col cols="12" md="6" lg="8" class="mt-5">
+                <p class="mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus minima hic repudiandae eaque fuga ab incidunt, illo esse soluta sed maxime consequuntur nesciunt dignissimos accusamus amet iusto voluptates consequatur?</p>
+            </b-col>
+            <b-col cols="12" md="6" lg="4" class="mt-5">
+                <img src="@/assets/logo.png" alt="">
+            </b-col>
+        </b-row>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Grid'
+}
+</script>
