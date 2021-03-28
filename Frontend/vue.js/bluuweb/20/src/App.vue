08af22271b0510ee0d51a55bc4677a843852e84b
Julián Swerdlin <julianswer36@gmail.com>
2021-03-28 13:29:40 -0300

    Update App.vue

--- Frontend/vue.js/bluuweb/20/src/App.vue
+++ Frontend/vue.js/bluuweb/20/src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/servicios">Servicios</router-link>
+      <router-link to="/servicios">Servicios</router-link> |
     </div>
     <router-view/>
   </div>
