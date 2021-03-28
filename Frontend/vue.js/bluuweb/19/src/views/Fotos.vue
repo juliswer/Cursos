e91f60b45c41e641ea3887c6330cedc86e4c5803
Julián Swerdlin <julianswer36@gmail.com>
2021-03-28 09:34:00 -0300

    Add files via upload

---
+++ Frontend/vue.js/bluuweb/19/src/views/Fotos.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Galeria de fotos</h1>
+        <Foto />
+    </div>
+</template>
+
+<script>
+import Foto from "@/components/Foto.vue"
+
+export default {
+    components: {
+        Foto
+    }
+}
+</script>
