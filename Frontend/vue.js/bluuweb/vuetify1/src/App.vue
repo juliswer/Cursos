65c43dc768215b6af3b34230ee3a3d39a3a18ff3
Julián Swerdlin <julianswer36@gmail.com>
2021-04-03 15:45:30 -0300

    Add files via upload

---
+++ Frontend/vue.js/bluuweb/vuetify1/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+        <v-content>
+          <v-container>
+            <router-view/>
+              <v-dialog v-model="loading.estado" hide-overlay persistent width="300"></v-dialog>
+              <v-card color="primary" dark></v-card>
+              <v-card-text>{{loading.titulo}}
+              <v-progress-linear indeterminate color="white" class="mb-0"></v-progress-linear>
+              </v-card-text>
+          </v-container>
+        </v-content>
+  </v-app>
+</template>
+
+<script>
+
+import { mapState } from "vuex";
+
+export default {
+  name: 'App',
+
+  components: {
+  
+  },
+
+  data: () => ({
+    //
+  }),
+  computed: {
+    ...mapState(['loading'])
+  }
+};
+</script>
