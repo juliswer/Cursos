c095a97855dca83ef0cc1bb03f471e61acf8777d
Julián Swerdlin <julianswer36@gmail.com>
2021-04-05 12:54:48 -0300

    Add files via upload

---
+++ Frontend/React/fazt/tasksApp/src/components/Task.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+
+class Task extends Component {
+    
+    StyleCompleted() {
+        return {
+            fontSize: '20px',
+            color: this.props.task.done ? 'gray' : 'black',
+            textDecoration: this.props.task.done ? 'line-through' : 'none'
+        }
+    }
+    
+    render(){
+        const {task} = this.props;
+        return <div style={this.StyleCompleted()}>
+            {task.title} - {task.description} - {task.done} - {task.id}
+            <input type="checkbox"/>
+            <button style={btnDelete}>
+                X
+            </button>
+        </div>
+    }
+}
+
+const btnDelete = {
+    fontSize: '18px',
+    background: '#ea2027',
+    color: '#fff',
+    border: 'none',
+    padding: '10px 15px',
+    borderRadius: '50%',
+    cursor: 'pointer'
+}
+
+export default Task;  
