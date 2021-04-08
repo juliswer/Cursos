63542dbf10c40a7169428b99e68af07b26c779e0
Julián Swerdlin <julianswer36@gmail.com>
2021-04-08 15:34:52 -0300

    Add files via upload

---
+++ Frontend/React/bluuweb/fundamentos/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Contador from './components/Contador.js'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola mundo</h1>
+      <Contador/>
+    </div>
+  );
+}
+
+export default App;
