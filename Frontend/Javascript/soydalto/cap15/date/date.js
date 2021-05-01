b9385050122712905815c7ccd3d4cc36ddaee8b9
Julián Swerdlin <julianswer36@gmail.com>
2021-05-01 19:47:30 -0300

    Add files via upload

---
+++ Frontend/Javascript/soydalto/cap15/date/date.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const fecha = Date();
+
+console.log(fecha.getYear());
+console.log(fecha.getDay());
+console.log(fecha.getMonth());
+console.log(fecha.getDate());
+console.log(fecha.getHours());
+console.log(fecha.getMinutes());
+console.log(fecha.getSeconds());
