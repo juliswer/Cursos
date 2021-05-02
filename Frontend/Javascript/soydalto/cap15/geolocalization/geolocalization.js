d4b724d9d183f26a1081e0b296d4a593c4216fe2
Julián Swerdlin <julianswer36@gmail.com>
2021-05-02 16:34:03 -0300

    Add files via upload

---
+++ Frontend/Javascript/soydalto/cap15/geolocalization/geolocalization.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const geolocation = navigator.geolocation;
+
+const posicion = (pos) => {
+    console.log(pos);
+}
+
+const err = () => console.log(e);
+
+const options = {
+    maximumAge: 0,
+    timeout: 3000,
+    enableHightAccuracy: true
+}
+
+geolocation.getCurrentPosition(posicion, err, options);
