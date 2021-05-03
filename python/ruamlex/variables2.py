20fd64e51626fc61f0aeeb9dcc13577a212758bb
Julián Swerdlin <julianswer36@gmail.com>
2021-05-03 19:42:34 -0300

    Update variables2.py

--- python/ruamlex/variables2.py
+++ python/ruamlex/variables2.py
@@ -14,7 +14,7 @@ def recoger_valores():
     precio = float(input("Introduzca el precio de la subscripcion: "))
     num_suscriptores_mes_uno = int(input("Introduzca numero subs mensual 1: "))
     num_suscriptores_mes_dos = int(input("Introduzca numero subs mensual 2: "))
-    calcular_importe(num_suscriptores_mes_uno, precio, "eneroo")
+    calcular_importe(num_suscriptores_mes_uno, precio, "enero")
     calcular_importe(num_suscriptores_mes_uno, precio, "febrero")
 
 recoger_valores()
