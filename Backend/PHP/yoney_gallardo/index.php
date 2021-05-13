08b54f9e7bc515743fde2a5a896be342e3b15803
Julián Swerdlin <julianswer36@gmail.com>
2021-05-13 19:38:25 -0300

    Add files via upload

---
+++ Backend/PHP/yoney_gallardo/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Hola a todos en PHP</h3>
+</body>
+</html>
+
+<?php 
+    // comentario en linea
+    /*
+    comentario en bloque
+    */
+    $a = "<h3>Hola a todos</h3>";
+    $b = 456;
+    $dia = 13;
+    echo $dia;
+    echo gettype($dia);
+    echo var_dump($dia);
+    echo $a;
+    echo "<h3>Esto es un codigo de php</h3>";
+    echo "ESTO ES MI PRIMER CODIGO EN PHP";
+?>
