bbad71e0d89b37b50437c5712abd1f0383dac0dc
Julián Swerdlin <julianswer36@gmail.com>
2021-05-15 21:15:20 -0300

    Add files via upload

--- Backend/PHP/yoney_gallardo/index.php
+++ Backend/PHP/yoney_gallardo/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Curso PHP</title>
 </head>
 <body>
     <h3>Hola a todos en PHP</h3>
@@ -19,10 +19,36 @@
     $a = "<h3>Hola a todos</h3>";
     $b = 456;
     $dia = 13;
-    echo $dia;
+    $nombre = "Pepe";
+    $apellido = "Gonzales";
+    echo "<p>$nombre</p>"; 
+    echo "<p>$dia</p>";
     echo gettype($dia);
     echo var_dump($dia);
-    echo $a;
+    echo "<p>$a</p>";
     echo "<h3>Esto es un codigo de php</h3>";
-    echo "ESTO ES MI PRIMER CODIGO EN PHP";
+    echo "<h3>ESTO ES MI PRIMER CODIGO EN PHP</h3>";
+    echo "<p>-45</p>";
+    echo "<p>-180004</p>";
+    echo "\\";
+    echo "<p>personas de la \"tercera\" edad.</p>";
+    echo "El precio es 15 \$us";
+    echo $nombre . " - " . $apellido;
+    echo "<p>".$nombre . " " . $apellido ."</p>";
+    echo "<a href=\"https://google.com\">" . $nombre . "</a>" . $apellido;
+    $c = FALSE;
+    $d = 10;
+    $e = 11;
+    if ($d < 11) {
+        echo "es menor";
+    }
+    if ($e < 9) {
+        echo "no es menor";
+    }
+    $dia1 = "Lunes";
+    $dia2 = "Martes";
+
+    $f = (int)$d;
+
+    var_dump($f);
 ?>
