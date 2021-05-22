ac323e1ed26c73dda6db01cfe4592ca10c8b654c
Julián Swerdlin <julianswer36@gmail.com>
2021-05-22 11:30:10 -0300

    Add files via upload

---
+++ Backend/PHP/yoney_gallardo/index8.php
@@ -0,0 +1,8 @@
+<?php 
+    require('index8.header.php');
+    include('index8.header.php');
+    require('index8.footer.php');
+    include('index8.footer.php');
+    require_once();
+    include_once();
+?>
