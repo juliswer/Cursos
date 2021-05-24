e0630f07e8cdf835cdcffd4ad6e38d747ff207bf
Julián Swerdlin <julianswer36@gmail.com>
2021-05-24 10:14:25 -0300

    Add files via upload

---
+++ Backend/Node.js/vida_mrr/02/02.js
@@ -0,0 +1,10 @@
+var path = require("path");
+var a = 5;
+var b = 10;
+console.log(a + b);
+console.log("hola");
+console.log(`El resultado es ${a + b}`);
+console.log(__dirname);
+console.log(__filename);
+// console.log(path);
+console.log(path.basename(__filename));
