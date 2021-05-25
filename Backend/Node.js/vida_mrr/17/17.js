3065b0e75f9ccf127f932edca0672edf37033769
Julián Swerdlin <julianswer36@gmail.com>
2021-05-25 17:13:08 -0300

    Add files via upload

---
+++ Backend/Node.js/vida_mrr/17/17.js
@@ -0,0 +1,28 @@
+const http = require('http');
+
+const options = {
+    hostname: 'localhost',
+    port: 80,
+    path: './17.js',
+    method: 'GET'
+};
+
+const req = http.request(options, res => {
+    console.log(`status code: ${res.statusCode}`);
+    console.log('Headers: %j', res.headers);
+    let body = '';
+    res.on('data', chunk => {
+        body += chunk;
+    });
+
+    res.on('end', () => {
+        console.log('\n\nResultados');
+        console.log(body);
+    })
+});
+
+req.on('error', err => {
+
+})
+
+req.end();
