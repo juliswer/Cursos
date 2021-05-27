b5ee9d25b9ababe092bc0f111995058e954b51da
Julián Swerdlin <julianswer36@gmail.com>
2021-05-27 16:30:05 -0300

    Add files via upload

---
+++ Backend/Node.js/vida_mrr/29/29.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index', {titulo: 'Mi primera app', nombre: 'Julian Swerdlin'});
+});
+
+app.listen(3002, () => {
+    console.log('Servidor iniciado...');
+})
